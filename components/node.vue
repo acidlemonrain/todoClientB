b90413ff74a91fad9b0582697374861eef32c6c9
hyc <992273994@qq.com>
2019-07-14 19:20:12 +0800

    'collapseadded'

--- components/node.vue
+++ components/node.vue
@@ -1,10 +1,11 @@
 <template>
-  <b-container>
-    <p :style="{'font-size':`${(30-dep*3)}px`}">{{node.name}}</p>
-    <p>
+  <div>
+    <b-button v-b-toggle="'collapse-'+node.id" class="m-1">{{node.name}} sadds</b-button>
+    <b-collapse :id="'collapse-'+node.id">
+      <div v-for="file in node.files" :key="file.id">{{file.name}}</div>
       <node v-for="child in  node.children" :key="child.id" :node="child" :dep="dep+1" />
-    </p>
-  </b-container>
+    </b-collapse>
+  </div>
 </template>
 
 <script>
